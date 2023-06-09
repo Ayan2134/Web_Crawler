9ddd542796e6124cd8661d4bdc6e0ba18e8378b2
Ayan <ayantanwar04@gmail.com>
2023-06-10 03:10:36 +0530

    Corrected Files

--- search_crawler.py
+++ search_crawler.py
@@ -1,25 +1,26 @@
 """
-Takes in a string and search that string(case insensitive) inside the webpages if found a match then returns the link for it
+Takes in a string and search that string(case insensitive) inside the webpage source code if found a match then returns the link for it
 """
 import sys
 import argparse
 import requests
+import lxml
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 
-int_links=0
-ext_links=0
 visited_links = set()
 internal_links = set()
 external_links = set()
 
-def crawl(url, start_domain, search_string, depth=1, max_depth=None):
+def crawl(url, start_domain, search_string,depth,max_depth):
+    if max_depth!=None :
+        if depth>max_depth :
+            return 
     """
     Crawls the given URL and extracts links from the page.
     Recursively follows the links within the same domain up to the maximum depth threshold.
     Searches for the specified string within the page content.
     """
-
     visited_links.add(url)
     print("Crawling:", url)
 
@@ -43,7 +44,7 @@ def crawl(url, start_domain, search_string, depth=1, max_depth=None):
                 next_url = href
             else:
                 next_url = urljoin(url, href)
-            process_url(next_url, start_domain, search_string, depth + 1, max_depth)
+            process_url(next_url, start_domain, search_string,depth,max_depth)
         
         if "src" in link.attrs:
             src = link["src"]
@@ -51,7 +52,7 @@ def crawl(url, start_domain, search_string, depth=1, max_depth=None):
                 next_url = src
             else:
                 next_url = urljoin(url, src)
-            process_url(next_url, start_domain, search_string, depth + 1, max_depth)
+            process_url(next_url, start_domain, search_string,depth,max_depth)
        
         if "action" in link.attrs :
             action = link["action"]
@@ -59,29 +60,26 @@ def crawl(url, start_domain, search_string, depth=1, max_depth=None):
                 next_url = action
             else :
                 next_url = urljoin(url, action)
-            process_url(next_url, start_domain, depth+1, max_depth)
+            process_url(next_url, start_domain,search_string,depth,max_depth)
             
-def process_url(url, start_domain, search_string, depth=1, max_depth=None):
+def process_url(url, start_domain, search_string,depth,max_depth):
     """
     Processes the URL and determines whether to crawl or consider it as an internal or external link.
     Searches for the specified string within the page content.
     """
-    global int_links 
-    global ext_links
     parsed_url = urlparse(url)
     domain = parsed_url.netloc
     if domain == start_domain:
         if url not in visited_links:
             if max_depth is None or depth <= max_depth:
-                int_links+=1
                 internal_links.add(url)
-                crawl(url, start_domain, search_string, depth, max_depth)
+                crawl(url, start_domain, search_string,depth+1,max_depth)
     else:
-        if url not in external_links :
-            ext_links+=1
-            external_links.add(url)
+        if depth <=max_depth or max_depth is None :
+            if url not in external_links :
+                external_links.add(url)
 
-def search_string_in_page(url, search_string):
+def search_string_source_code(url, search_string):
     """
     Fetches the HTML content of the page at the specified URL and searches for the string within the content.
     Returns True if the string is found, False otherwise.
@@ -113,33 +111,31 @@ if __name__ == "__main__":
     parsed_start_url = urlparse(start_url)
     start_domain = parsed_start_url.netloc
 
-    if threshold is None:
-        crawl(start_url, start_domain, search_string)
-    else:
-        crawl(start_url, start_domain, search_string, max_depth=threshold)
+    crawl(start_url, start_domain, search_string,1,threshold)
 
     print("\nInternal links:")
     for link in internal_links:
         print(link)
-    print(f"\nTotal Internal Links = {int_links} \n")
+    print(f"\nTotal Internal Links = {len(internal_links)} \n")
 
     print("\nExternal links:")
     for link in external_links:
         print(link)
-    print(f"\nTotal External Links = {ext_links} \n")
+    print(f"\nTotal External Links = {len(external_links)} \n")
     if search_string:
         print("\nSearch results:")
         found = False
         for link in internal_links:
-            if search_string_in_page(link, search_string):
+            if search_string_source_code(link, search_string):
                 found = True
                 print(f"Search result in Internal Link: {link}")
         if not found:
             print("\nNo Internal link found with the search string.\n")
         found=False
         for link in external_links :
-            if search_string_in_page(link,search_string) :
+            if search_string_source_code(link,search_string) :
                 found=True
                 print(f"Search result in External Link: {link}")
         if not found :
             print("\nNo External link with the search string.\n")
+       
--- web_crawler.py
+++ web_crawler.py
@@ -10,9 +10,10 @@ define it as a set using set() . Used set here to avoid repeating links in recur
 visited_links = set() #to store already visited links 
 internal_links = set() #to group internal links which will be crawled further
 external_links = set() #to store external links(links with same domain) which will just be mentioned and not crawled 
-int_links=0
-ext_links=0
-def crawl(url, start_domain, depth=1, max_depth=None):
+def crawl(url, start_domain, depth, max_depth):
+    if max_depth != None :
+        if depth>max_depth :
+            return
     """
     Crawls the given URL and extracts links from the page.
     Recursively follows the links within the same domain up to the maximum depth threshold.
@@ -50,7 +51,7 @@ def crawl(url, start_domain, depth=1, max_depth=None):
                 urljoin is used instead of simply concatenating the strings because if the url has already some relative part attached
                 to it then it removes that and attach the relative url provided by user as argument and returns it as a proper url
                 """
-            process_url(next_url, start_domain, depth + 1, max_depth)
+            process_url(next_url, start_domain, depth , max_depth)
         
         if "src" in link.attrs:
             src = link["src"]
@@ -58,7 +59,7 @@ def crawl(url, start_domain, depth=1, max_depth=None):
                 next_url = src
             else:
                 next_url = urljoin(url, src)
-            process_url(next_url, start_domain, depth + 1, max_depth)
+            process_url(next_url, start_domain, depth , max_depth)
         
         if "action" in link.attrs :
             action = link["action"]
@@ -66,9 +67,9 @@ def crawl(url, start_domain, depth=1, max_depth=None):
                 next_url = action
             else :
                 next_url = urljoin(url, action)
-            process_url(next_url, start_domain, depth+1, max_depth)
+            process_url(next_url, start_domain, depth, max_depth)
 
-def process_url(url, start_domain, depth=1, max_depth=None):
+def process_url(url, start_domain, depth, max_depth):
     """
     Processes the URL and determines whether to crawl or consider it as an internal or external link.
     """
@@ -80,14 +81,9 @@ def process_url(url, start_domain, depth=1, max_depth=None):
         if url not in visited_links:
             if max_depth is None or depth <= max_depth:
                 internal_links.add(url)
-                int_links+=1
-                crawl(url, start_domain, depth, max_depth)
+                crawl(url, start_domain, depth+1, max_depth)
     else:
-        if url not in external_links : #to count only unique external links
-            """ added this if statement as there were some repetitions in external links it 
-            was increasing ext_links counter for same links. It didn't affect external_links set because 
-            a set automatically rejects repetitions"""
-            ext_links+=1
+        if url not in external_links : #to count only unique external links although it doesn't matter as it is a set
             external_links.add(url)
 
 if __name__ == "__main__":
@@ -95,24 +91,20 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--url", help="Start URL", required=True)
     parser.add_argument("-t", "--threshold", help="Recursion Depth Threshold", type=int, default=None)
     args = parser.parse_args()
-
     start_url = args.url
     threshold = args.threshold
-
     parsed_start_url = urlparse(start_url)
     start_domain = parsed_start_url.netloc
+    if threshold>0 :
+        crawl(start_url, start_domain, 1,threshold)
+        print("\nInternal links:")
+        for link in internal_links:
+            print(link)
+        print(f"\nTotal Internal Links = {len(internal_links)} \n")
+        print("\nExternal links:")
+        for link in external_links:
+            print(link)
+        print(f"\nTotal External Links = {len(external_links)} \n")
 
-    if threshold is None:
-        crawl(start_url, start_domain)
-    else:
-        crawl(start_url, start_domain, max_depth=threshold)
-
-    print("\nInternal links:")
-    for link in internal_links:
-        print(link)
-    print(f"\nTotal Internal Links = {int_links} \n")
-    print("\nExternal links:")
-    for link in external_links:
-        print(link)
-    print(f"\nTotal External Links = {ext_links} \n")
-
+    else :
+        print("Invalid Threshhold")
