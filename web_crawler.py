4ff6fb020d816263433f32c84340decccd270dda
Ayan <ayantanwar04@gmail.com>
2023-06-14 17:43:58 +0530

    Final version

--- web_crawler.py
+++ web_crawler.py
@@ -1,3 +1,4 @@
+#internal and external links are saved in sets so no repetition
 import sys
 import argparse
 import requests
@@ -19,7 +20,6 @@ def crawl(url, start_domain, depth, max_depth):
     Crawls the given URL and extracts links from the page.
     Recursively follows the links within the same domain up to the maximum depth threshold.
     """
-
     visited_links.add(url)
     print("Crawling:", url)
 
@@ -61,14 +61,6 @@ def crawl(url, start_domain, depth, max_depth):
             else:
                 next_url = urljoin(url, src)
             process_url(next_url, start_domain, depth , max_depth)
-        
-        if "action" in link.attrs :
-            action = link["action"]
-            if action.startswith("http") :
-                next_url = action
-            else :
-                next_url = urljoin(url, action)
-            process_url(next_url, start_domain, depth, max_depth)
 
 def process_url(url, start_domain, depth, max_depth):
     """
@@ -82,41 +74,55 @@ def process_url(url, start_domain, depth, max_depth):
                 internal_links.add(url)
                 crawl(url, start_domain, depth+1, max_depth)
     else:
-        if url not in external_links : #to count only unique external links although it doesn't matter as it is a set
-            external_links.add(url)
-    
-def search_string_source_code(url, search_string):
-    """
-    Fetches the HTML content of the page at the specified URL and searches for the string within the content.
-    Returns True if the string is found, False otherwise.
-    """
+       external_links.add(url)
 
-    try:
-        response = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
-        return False
+def group_domain() :  
+    domain=set()  
+    for link in external_links :
+        parsed_url=urlparse(link)
+        if parsed_url.netloc:
+            domain.add(parsed_url.netloc)
+    for domain_name in domain :
+        print(f"\n{domain_name}\n")
+        for link in external_links :
+            parsed_url=urlparse(link)
+            if domain_name==parsed_url.netloc :
+                print("\t",link)
 
-    if response.status_code != 200:
-        return False
-
-    page_text = response.text
-    return search_string.lower() in page_text.lower() # It does case insensitive searching
-
+def sort_extensions(links) :
+    extension=set()
+    for link in links:    
+        parsed_link = urlparse(link)
+        path = parsed_link.path
+        extension.add(os.path.splitext(path)[1])
+    for ext in extension:
+        if ext!="":
+            print(f"\n{ext}\n")
+        else :
+            print("\nMiscellaneous\n")
+        for link in links :
+            parsed = urlparse(link)
+            link_path = parsed.path
+            link_ext=os.path.splitext(link_path)[1]
+            if link_ext==ext :
+                print("\t",link)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Web Crawler")
     parser.add_argument("-u", "--url", help="Start URL", required=True)
     parser.add_argument("-t", "--threshold", help="Recursion Depth Threshold", type=int, default=None)
-    parser.add_argument("-s", "--search", help="Search String", default="")
     parser.add_argument("-o","--output", help="Create an output file ", default="")
+    parser.add_argument("-s","--simple",help="Simplify the results that we get",action="store_true")
+    parser.add_argument("-e","--ext_sort",help="Sort the external links based on extensioin",action="store_true")
     args = parser.parse_args()
     start_url = args.url
     threshold = args.threshold
     output_file=args.output
-    search_string = args.search
+    simple=args.simple 
+    ext_sort=args.ext_sort
     parsed_start_url = urlparse(start_url)
     start_domain = parsed_start_url.netloc
+    flag=False #to check if output file was made or not
     if threshold is None or threshold>0 :
         if output_file :
             if os.path.exists(output_file) :
@@ -124,31 +130,31 @@ if __name__ == "__main__":
             else :
                 path=f"./{output_file}"
                 sys.stdout=open(path,"w")
+                flag=True
         crawl(start_url, start_domain, 1,threshold)
-        print("\nInternal links:")
-        for link in internal_links:
-            print(link)
+        print("\nInternal links:\n")
+        if simple==True : #getting simplified output
+            for link in internal_links:
+                print("\t",link)
+        else :
+            sort_extensions(internal_links)
         print(f"\nTotal Internal Links = {len(internal_links)} \n")
         print("\nExternal links:")
-        for link in external_links:
-            print(link)
-        print(f"\nTotal External Links = {len(external_links)} \n")
-        if search_string:
-            print("\nSearch results:")
-            found = False
-            for link in internal_links:
-                if search_string_source_code(link, search_string):
-                    found = True
-                    print(f"Search result in Internal Link: {link}")
-            if not found:
-                print("\nNo Internal link found with the search string.\n")
-            found=False
+        if simple==True : #getting simplified output
             for link in external_links :
-                if search_string_source_code(link,search_string) :
-                    found=True
-                    print(f"Search result in External Link: {link}")
-            if not found :
-                print("\nNo External link with the search string.\n")
-
+                print("\t",link)
+        else :
+            group_domain()
+        if ext_sort==True :
+            print("\nExternal links sorted based on extensions :\n")
+            sort_extensions(external_links)
+        print(f"\nTotal External Links = {len(external_links)} \n")
+        print(f"\nTotal Links = {len(internal_links)+len(external_links)} \n")
+        if flag :
+            sys.stdout=sys.__stdout__ #restoring sys,stdout to its original state, sys.__stdout__ refers to original output stream 
+            print(f"\nTotal Internal Links Found = {len(internal_links)} \n")
+            print(f"\nTotal External Links Found = {len(external_links)} \n")
+            print(f"\nTotal Links Found = {len(internal_links)+len(external_links)} \n")
+        
     else :
         print("Invalid Threshhold")
